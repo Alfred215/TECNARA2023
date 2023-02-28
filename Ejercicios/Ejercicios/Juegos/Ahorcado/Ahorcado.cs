5449f63e57d29eed6c70fcc76020762e2734cfe7
Laura Yus Sancho <lyus@efor.es>
2023-02-28 16:54:20 +0100

    Me meo en las bragas que dibujo tan chulo

--- Ejercicios/Ejercicios/Juegos/Ahorcado/Ahorcado.cs
+++ Ejercicios/Ejercicios/Juegos/Ahorcado/Ahorcado.cs
@@ -42,7 +42,10 @@ namespace Ejercicios.Juegos.Ahorcado
             while (letrasCorrectas < palabraAdivinar.Length && intentos < 6)
             {
                 // Mostrar las letras adivinadas hasta el momento
-                Console.WriteLine("Palabra: " + new string(letrasAdivinadas));
+                Console.WriteLine("Palabra: " + string.Join(" ", letrasAdivinadas));
+
+                // Mostrar el dibujo del ahorcado
+                Console.WriteLine(DibujarAhorcado(intentos));
 
                 // Pedir al usuario que adivine una letra
                 Console.Write("Adivina una letra: ");
@@ -76,9 +79,26 @@ namespace Ejercicios.Juegos.Ahorcado
             else
             {
                 Console.WriteLine("¡Lo siento! La palabra era \"" + palabraAdivinar + "\"");
+                Console.WriteLine(DibujarAhorcado(intentos));
             }
 
             Console.ReadKey();
+        }
+
+        static string DibujarAhorcado(int intentos)
+        {
+            // Dibujo del ahorcado
+            string[] dibujo = {
+                "   _____",
+                "   |   |",
+                "   |   " + (intentos > 0 ? "O" : ""),
+                "   |  " + (intentos > 1 ? "/" : " ") + (intentos > 2 ? "|" : "") + (intentos > 3 ? "\\" : ""),
+                "   |  " + (intentos > 4 ? "/" : "") + " " + (intentos > 5 ? "\\" : ""),
+                "___|___"
+                };
+
+            // Unir las líneas del dibujo en un solo string
+            return string.Join("\n", dibujo);
         }
     }
 }
