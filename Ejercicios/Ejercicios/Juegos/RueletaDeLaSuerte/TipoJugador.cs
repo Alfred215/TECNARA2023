2574a3854b7f09e53c744d2d13d5b1ad28355cac
Alfred215 <alfredoarpal@gmail.com>
2023-03-08 08:41:57 +0100

    merge

---
+++ Ejercicios/Ejercicios/Juegos/RueletaDeLaSuerte/TipoJugador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios.Juegos.RueletaDeLaSuerte
+{
+    public enum TipoJugador
+    {
+        Ganador = 0,
+        Eliminado = 1,
+        Juega = 2,
+    }
+}
