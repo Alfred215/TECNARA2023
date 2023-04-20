01aa34fe25a8c227fa9bedee15f9772fc06d300f
Alfred215 <alfredoarpal@gmail.com>
2023-04-20 16:56:23 +0200

    Añadir Entidad y DTOs de area y funcion

---
+++ Ejercicios/Entities/DTO/AreaDTOs/AreaMiniDTO.cs
@@ -0,0 +1,23 @@
+﻿using Infraestructure.Enumerables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.DTO.AreaDTOs
+{
+    public class AreaMiniDTO 
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public int Tamaño { get; set; }
+
+        #region Hospital
+        public Guid HospitalId { get; set; }
+        public string HospitalNombre { get; set; }
+        public LocalizacionType HospitalLocalizacion { get; set; }
+        public string HospitalEspecialidades { get; set; }
+        #endregion
+    }
+}
---
+++ Ejercicios/Entities/DTO/FuncionDTOs/FuncionMiniDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.DTO.FuncionDTOs
+{
+    public class FuncionMiniDTO
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+
+        #region AREA
+        public Guid AreaId { get; set; }
+        public string AreaNombre { get; set; }
+        public int AreaTamaño { get; set; } 
+        #endregion
+    }
+}
