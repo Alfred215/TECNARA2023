4d257c1998304f0255a22e7b2298961caf97c07e
Alfred215 <alfredoarpal@gmail.com>
2023-05-17 08:38:30 +0200

    Select

---
+++ Ejercicios/Entities/Enumerables/TipoPersona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Enumerables
+{
+    public enum TipoPersona
+    {
+        [Description("Adulto")]
+        Adult = 0,
+
+        [Description("Anciano")]
+        OldPerson = 1,
+
+        [Description("Joven")]
+        Teen = 2,
+
+        [Description("Bebe")]
+        Baby = 3,
+    }
+}
