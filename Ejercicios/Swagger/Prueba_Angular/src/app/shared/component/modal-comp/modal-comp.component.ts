98f7e5d9409785ef565e67fc70b2b0a5b7548c43
Laura Yus Sancho <lyus@efor.es>
2023-05-23 13:20:55 +0200

    Añadir color al nav seleccionado

--- Ejercicios/Swagger/Prueba_Angular/src/app/shared/component/modal-comp/modal-comp.component.ts
+++ Ejercicios/Swagger/Prueba_Angular/src/app/shared/component/modal-comp/modal-comp.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-modal-comp',
   templateUrl: './modal-comp.component.html',
   styleUrls: ['./modal-comp.component.scss']
 })
-export class ModalCompComponent {
+export class ModalCompComponent implements OnInit {
 
+  @Input('path') path: string = '';
+  @Input('selected') selected: boolean = false;
+
+  currentPath: string = '';
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.router.events
+      .pipe()
+      .subscribe(() => {
+        this.comprobarRuta();
+      });
+  }
+
+  comprobarRuta() {
+    
+    const rutaActual = this.router.url;
+    const etiqueta1 = document.getElementById('persons-link') as HTMLElement;;
+    const etiqueta2 = document.getElementById('clients-link') as HTMLElement;;
+
+    if (rutaActual === '/persona') etiqueta1.style.color = '#ffcc00';
+    if (rutaActual === '/cliente/list') etiqueta2.style.color = '#ffcc00'; 
+    
+  }
 }
